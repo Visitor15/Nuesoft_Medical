94b6ce9c7b3430ae4e9debda75b581b91e696081
visitor15 <nick.champagne@metago.net>
2013-10-06 16:48:33 -0400

    Added a NuesoftUser class.

---
+++ src/com/mobile/nuesoft/NuesoftUser.java
@@ -0,0 +1,109 @@
+package com.mobile.nuesoft;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
+
+import android.util.Base64;
+import android.util.Log;
+
+/*
+ * Simple class used to represent a user during registration or login
+ */
+public class NuesoftUser {
+	
+	public static final String TAG = "NuesoftUser";
+
+	private String userName = "";
+	private String string64PSWRD = "";
+
+	private byte[] salt;
+	private byte[] encryptedPSWRD;
+
+	public NuesoftUser() {}
+	
+	public void setUserName(final String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public void setString64PSWRD(final String string64PSWRD) {
+		this.string64PSWRD = string64PSWRD;
+	}
+
+	public String getString64PSWRD() {
+		return this.string64PSWRD;
+	}
+
+	public void setSalt(final byte[] salt) {
+		this.salt = salt;
+	}
+	
+	public byte[] getSalt() {
+		return this.salt;
+	}
+
+	public void setEncryptedPassword(final byte[] encryptedPSWRD) {
+		this.encryptedPSWRD = encryptedPSWRD;
+	}
+	
+	public byte[] getEncryptedPassword() {
+		return this.encryptedPSWRD;
+	}
+
+	@Override
+	public String toString() {
+		String val = "";
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		ObjectOutputStream objOutStream;
+		
+		try {
+			objOutStream = new ObjectOutputStream(outStream);
+			
+			objOutStream.writeInt(salt.length);
+			objOutStream.write(salt, 0, salt.length);
+			objOutStream.writeInt(encryptedPSWRD.length);
+			objOutStream.write(encryptedPSWRD, 0, encryptedPSWRD.length);
+			
+			objOutStream.flush();
+			
+			val = Base64.encodeToString(outStream.toByteArray(), Base64.DEFAULT);
+		} catch (IOException e) {
+			return val;
+		}
+
+		return val;
+	}
+	
+	public static NuesoftUser fromString(final String string) {
+		final byte[] byteVal = Base64.decode(string, Base64.DEFAULT);
+		final ByteArrayInputStream is = new ByteArrayInputStream(byteVal);
+		final ObjectInputStream in;
+		int length = 0;
+		NuesoftUser user = new NuesoftUser();
+		
+		try {
+	        in = new ObjectInputStream(is);
+	        
+	        length = in.readInt();
+	        user.setSalt(new byte[length]);
+	        in.read(user.salt);
+	        
+	       	length = in.readInt();
+	       	user.setEncryptedPassword(new byte[length]);
+	       	in.read(user.encryptedPSWRD);
+        } catch (StreamCorruptedException e) {
+	        e.printStackTrace();
+        } catch (IOException e) {
+	        e.printStackTrace();
+        }
+		
+		return user;
+	}
+}
