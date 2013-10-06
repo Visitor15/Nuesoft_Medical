98510fe4381ba181dd47cb27e4417b8198c8043b
visitor15 <nick.champagne@metago.net>
2013-10-06 16:50:47 -0400

    Adding a RegisterUserJob class and a few methods in the Util class for encryption.

---
+++ src/com/mobile/nuesoft/jobs/RegisterUserJob.java
@@ -0,0 +1,76 @@
+package com.mobile.nuesoft.jobs;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.mobile.nuesoft.Nuesoft;
+import com.mobile.nuesoft.NuesoftUser;
+import com.mobile.nuesoft.preferences.NuesoftPreferences;
+import com.mobile.nuesoft.util.Util;
+
+public class RegisterUserJob extends AsyncTask<NuesoftUser, Void, Boolean> {
+
+	public static final String TAG = "RegisterUserJob";
+
+	private NuesoftUser mUser;
+
+	private boolean result = false;
+
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+	}
+
+	@Override
+	protected void onPostExecute(Boolean result) {
+		super.onPostExecute(result);
+
+		Bundle b = new Bundle();
+		b.putBoolean(NuesoftRegisteredUserEvent.RESULT_KEY, result);
+		b.putString(NuesoftRegisteredUserEvent.USER_KEY, mUser.toString());
+
+		NuesoftRegisteredUserEvent.broadcast(Nuesoft.getReference(), b);
+	}
+
+	@Override
+	protected void onProgressUpdate(Void... values) {
+		super.onProgressUpdate(values);
+	}
+
+	@Override
+	protected Boolean doInBackground(NuesoftUser... user) {
+		mUser = NuesoftPreferences.getInstance().getNuesoftUser(user[0].getUserName());
+		
+		if (mUser == null) {
+			try {
+				mUser = new NuesoftUser();
+				mUser.setUserName(user[0].getUserName());
+				mUser.setSalt(generateSalt());
+				mUser.setEncryptedPassword(Util.getEncryptedPassword(user[0].getString64PSWRD(), mUser.getSalt()));
+
+				// Saving user to preferences
+				result = NuesoftPreferences.getInstance().saveRegisteredUser(mUser);
+			} catch (final NoSuchAlgorithmException e) {
+
+			} catch (InvalidKeySpecException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+	private byte[] generateSalt() throws NoSuchAlgorithmException {
+		SecureRandom secRandom = SecureRandom.getInstance("SHA1PRNG");
+
+		byte[] salt = new byte[8];
+		secRandom.nextBytes(salt);
+
+		return salt;
+	}
+}
--- src/com/mobile/nuesoft/util/Util.java
+++ src/com/mobile/nuesoft/util/Util.java
@@ -1,13 +1,33 @@
 package com.mobile.nuesoft.util;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
 import android.content.Context;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 
 public class Util {
-	
+
 	public static int convertDpToPixel(final float dp, final Context context) {
 		final DisplayMetrics metrics = context.getResources().getDisplayMetrics();
 		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, metrics);
+	}
+
+	public static byte[] getEncryptedPassword(final String string64PSWRD, final byte[] salt)
+	        throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+		String algorithm = "PBKDF2WithHmacSHA1";
+		int derivedKeyLength = 160;
+		int iterations = 10000;
+
+		KeySpec spec = new PBEKeySpec(string64PSWRD.toCharArray(), salt, iterations, derivedKeyLength);
+		SecretKeyFactory factory = SecretKeyFactory.getInstance(algorithm);
+
+		return factory.generateSecret(spec).getEncoded();
 	}
 }
